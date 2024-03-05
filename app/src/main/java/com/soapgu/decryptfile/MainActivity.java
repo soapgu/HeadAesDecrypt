b6d33555cd0f51ebbb1598b84ebad28bffa8b5bc
soapgu <ghost_gu@163.com>
2024-03-05 16:17:50 +0800

    feat: 🎸 实现多种文件的解密处理和显示进度

--- app/src/main/java/com/soapgu/decryptfile/MainActivity.java
+++ app/src/main/java/com/soapgu/decryptfile/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.orhanobut.logger.Logger;
@@ -17,6 +18,8 @@ import java.io.OutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+import java.util.Date;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -40,7 +43,8 @@ import okhttp3.ResponseBody;
 public class MainActivity extends AppCompatActivity {
 
     OkHttpClient client;
-    TextView msg;
+    TextView msg,tv_complete;
+    ProgressBar progressBar;
     private final CompositeDisposable disposable = new CompositeDisposable();
 
     @Override
@@ -49,22 +53,43 @@ public class MainActivity extends AppCompatActivity {
         client = new OkHttpClient();
         setContentView(R.layout.activity_main);
         msg = findViewById(R.id.msg);
-        findViewById(R.id.btn_start).setOnClickListener( v -> {
-            Request request = new Request.Builder()
-                    .url("http://testing.heyshare.cn/download/encrypt_sample.jpg")
-                    .get()
-                    .build();
-            Call call = client.newCall(request);
-            String filePath = getExternalFilesDir(null) + "/image.jpg";
-            disposable.add( downloadFile(call)
-                            .flatMap( body -> decryptFile(body,filePath) )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe( file ->{
-                        msg.setText( file.getName() );
-                        //body.close();
-                    },
-                    throwable -> Logger.e(throwable,"download error")));
+        tv_complete = findViewById(R.id.tv_complete);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+
+        findViewById(R.id.btn_small_start).setOnClickListener( v -> {
+            productFile("encrypt_sample.jpg");
         } );
+
+        findViewById(R.id.btn_medium_start).setOnClickListener( v-> {
+            productFile( "encrypt_1G.mp4" );
+        } );
+
+        findViewById(R.id.btn_big_start).setOnClickListener( v-> {
+            productFile( "encrypt_film.mkv" );
+        } );
+    }
+
+    private void productFile( String fileName ){
+        tv_complete.setText("");
+        Date start = new Date();
+        String url = String.format("http://testing.heyshare.cn/download/%s",fileName);
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        Call call = client.newCall(request);
+        String filePath = getExternalFilesDir(null) + "/" + fileName;
+        disposable.add( downloadFile(call)
+                .flatMap( body -> decryptFile(body,filePath) )
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe( file ->{
+                            msg.setText( String.format( "文件%s解密完成",fileName ) );
+                            Date end = new Date();
+                            Long diff = end.getTime() - start.getTime() ;
+                            tv_complete.setText( String.format( "用时 %s 毫秒", diff ));
+                        },
+                        throwable -> Logger.e(throwable,"download error")));
     }
 
     private Single<ResponseBody> downloadFile( Call call ){
@@ -87,47 +112,53 @@ public class MainActivity extends AppCompatActivity {
 
     private Single<File> decryptFile( ResponseBody body, String filePath ){
         return Single.create( emitter -> {
+
             File file = new File(filePath);
             int headLength = 1024;
             if( body.contentLength() > 2048 ){
                 headLength += 16;
             }
-            byte[] header = new byte[headLength];
-            byte[] headerBuffer = new byte[headLength];
+
             byte[] buffer = new byte[4096];
             InputStream inputStream = body.byteStream();
             OutputStream outputStream = null;
             try {
+                this.runOnUiThread( ()-> progressBar.setProgress( 0 ));
                 long totalProcess = 0L;
                 outputStream = new FileOutputStream(file);
                 long skip = inputStream.skip(1024);
                 totalProcess += skip;
                 Logger.i( "Step 1: skip %d",skip );
-                int read =  inputStream.read( headerBuffer );
-                if( read == headLength ){
-                    header = headerBuffer;
-                } else {
-                    System.arraycopy(headerBuffer, 0, header, 0, read);
-                    headerBuffer = new byte[headLength - read];
-                    int remain =  inputStream.read( headerBuffer );
-                    Logger.i("read first %s, remain header expect:%s,actual %s", read,headLength - read,remain);
-                    System.arraycopy(headerBuffer,0,header,read,remain);
-                }
-                Logger.i( "Step 2: read header length %d",read );
-                totalProcess += read;
+                byte[] header = readHeader( inputStream , headLength );
+                Logger.i( "Step 2: read header length %d",headLength );
+                totalProcess += headLength;
                 byte[] decryptedBytes = decryptHeader( header );
                 Logger.i( "Step 3: decrypt header length %d",decryptedBytes.length );
-                Logger.i("decrypt content:%s",HexUtils.bytesToHex(decryptedBytes));
+                //Logger.i("decrypt content:%s",HexUtils.bytesToHex(decryptedBytes));
                 outputStream.write(decryptedBytes, 0, decryptedBytes.length);
+                int read;
+                long contentLength = body.contentLength();
+                long current = 0;
+                DecimalFormat df = new DecimalFormat("#.##");
                 do{
+                    current ++;
                     read = inputStream.read(buffer);
                     //Logger.i( "read file length:%d",read );
                     if( read > 0 ){
                         totalProcess += read;
                         outputStream.write( buffer,0,read );
+                        if( current % 10 == 0 ){
+                            double percent = totalProcess * 100.00  / contentLength;
+                            this.runOnUiThread( ()->{
+                                msg.setText( String.format("%s %%",df.format( percent )) );
+                                if( (int)percent > progressBar.getProgress() ) {
+                                    progressBar.setProgress((int)percent);
+                                }
+                            });
+                        }
                     }
                 }while (read > 0);
-                Logger.i( "process file content %s / %s ",totalProcess,body.contentLength() );
+                Logger.i( "process file content %s / %s ",totalProcess,contentLength );
                 outputStream.flush();
                 emitter.onSuccess( new File(filePath) );
             } catch (Exception e) {
@@ -146,6 +177,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 保证读取指定长度数据
+     * @param inputStream 输入流
+     * @param headLength 长度
+     * @return 数据数组
+     * @throws IOException
+     */
+    private byte[] readHeader( InputStream inputStream, int headLength ) throws IOException {
+        byte[] header = new byte[headLength];
+
+        int total = 0;
+        int remain = headLength;
+        do {
+            byte[] headerBuffer = new byte[remain];
+            int read = inputStream.read(headerBuffer);
+            System.arraycopy(headerBuffer,0,header,total,read);
+            total += read;
+            remain = headLength - total;
+            Logger.i("current read head %d / %d , remain %d", total, headLength,remain );
+        } while (remain > 0);
+        return header;
+    }
+
+    /**
+     * AES 解密
+     * 使用CBC模式&PKCS7Padding
+     * @param bytes 密文
+     * @return 明文
+     */
     private byte[] decryptHeader( byte[] bytes ){
         String ALGORITHM = "AES/CBC/PKCS7Padding";
         try {
